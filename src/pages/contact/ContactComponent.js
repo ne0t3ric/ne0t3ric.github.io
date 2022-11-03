7ed21073147ebdcafdba9f6e9ec1da0b6953a58a
HANG Eric <eric.hang@meteo.fr>
2022-11-03 13:08:06 -0400

    Fix unused vars

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -10,7 +10,7 @@ import BlogsImg from "./BlogsImg";
 import AddressImg from "./AddressImg";
 import { Fade } from "react-reveal";
 import "./ContactComponent.css";
-import { greeting, contactPageData } from "../../portfolio.js";
+import { /*greeting,*/ contactPageData } from "../../portfolio.js";
 
 const ContactData = contactPageData.contactSection;
 /*
